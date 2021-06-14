59007c39a2920909eb0b377cdfe4e5c6e6d74fc8
Antoine debes <antoinedebes@gmail.com>
2021-06-14 11:41:04 +0300

    step 3

--- index.js
+++ index.js
@@ -10,3 +10,44 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+app.get('/test', (req, res) => {
+    let data = {
+        status: 200,
+        message: 'Ok'
+    };
+
+    res.send(data);
+})
+
+
+app.get('/time', (req, res) => {
+    let date =  new Date;
+    let dateHour = date.getHours();
+    let dateMinut = date.getMinutes();
+   let newDate =  dateHour +':'+ dateMinut;
+    let data = {
+        status: 200,
+        message: newDate,
+    };
+    res.send(data);
+})
+// /**
+//  * Startup function
+//  */
+// function startApp () {
+//     process.stdin.on('data', onDataReceived);
+// }
+
+/**
+ * Tests the data that we received from the terminal
+ */
+
+// function onDataReceived () {
+//     console.log('yes');
+// }
+
+/**
+ * This is a call-back function that starts our app
+ */
+// startApp();
