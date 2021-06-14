0d0e43592413f103b7791b42becf8cd46aba322f
Antoine debes <antoinedebes@gmail.com>
2021-06-14 13:05:58 +0300

    Step 4

--- index.js
+++ index.js
@@ -32,6 +32,24 @@ app.get('/time', (req, res) => {
     };
     res.send(data);
 })
+
+app.get('/hello/:id?', (req, res,) => {
+    let id = req.params.id;
+    let response = {
+        status:200,
+        message:`Hello, ${id !== undefined? id : "User"}`}
+   res.send(response);
+})
+
+app.get('/search/:search?', (req, res) => {
+    let searchText = req.params.search;
+    let response = () => {
+        if(searchText !== undefined) return {status:200, message:"ok", data: searchText}
+        if(searchText === undefined) return {status:500, error:true, message:"you have to provide a search"}
+    }
+    res.send(response())
+})
+
 // /**
 //  * Startup function
 //  */
